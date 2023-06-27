d505ec3cd380aed833d96e2955776ec0c2c593ce
Kyle Rotich <kyle.rotich@pycs.co.ke>
2023-06-27 16:50:02 +0300

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace ProLang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           using var result = new Source("\\Documents\\code.txt");
+
+            var currentChar = result.CurrentChar();
+
+            var stringBuilder = new StringBuilder();
+
+            while (currentChar != Source.EOF){
+                stringBuilder.Append(currentChar);
+                currentChar = result.NextChar();
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+            
+        }
+    }
+}
